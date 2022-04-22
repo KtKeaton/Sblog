a8a716e762e6d23d72405ff46005b91af866ad13
KtKeaton <ktr01@protonmail.com>
2022-04-22 09:52:38 +0800

    VC Notes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get "notes", to: "notes#index"
+
   get "hello", to: "pages#main"
   get "about", to: "pages#about"
-
   get "users", to: "users#profile"
+
+
 end
