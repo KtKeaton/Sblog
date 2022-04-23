c12141ea3970fb1c984a71fe80ebc617be06395f
KtKeaton <ktr01@protonmail.com>
2022-04-23 23:31:37 +0800

    增加強參數，自動憑證修正

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,9 +1,27 @@
 class NotesController < ApplicationController
+  before_action :note_params
 
   def index
   end
 
   def new
+    @note = Note.new
+  end
+
+  def create
+    note = Note.new(note_params)
+    
+    if note.save
+      redirect_to "/notes"
+    else
+      #
+    end
+  end
+
+private
+
+  def note_params
+    params.permit(:title, :content)
   end
 
 end
