fe6fbb28c5bbb6104dc5ea37e62fde510537ac8d
KtKeaton <ktr01@protonmail.com>
2022-04-26 13:41:45 +0800

    Notes 假刪除商業邏輯

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,7 +2,5 @@ class Note < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
 
-  def self.available
-    where(deleted_at: nil)
-  end
+  default_scope { where(deleted_at: nil) }
 end
