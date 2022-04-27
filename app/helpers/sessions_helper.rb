f7ad9b2f7bb89938c14b6ba032205f5fabc2587b
KtKeaton <ktr01@protonmail.com>
2022-04-27 17:10:48 +0800

    Navbar與Footer

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,14 @@
 module SessionsHelper
+def user_sign_in?
+  session[:redoing] != nil
+end
+
+  def current_user
+    if session[:redoing]
+      User.find(session[:redoing])
+    else
+      nil
+    end
+  end
+
 end
